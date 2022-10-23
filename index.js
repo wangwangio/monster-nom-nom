1634904f19a033df19c81e70ff9686b2e1c7fc47
OksanaFedyukova <avseniya222@gmail.com>
2022-10-23 18:34:51 +0200

    pagina Game Over/ monster cambia colores

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-import './styles.css';
+/* let divCoders = document.getElementById("1");
+
+coders = [{ name: "Adrian", id: "1" }, { name: "Albert", id:"2" }, ...]
+divCoder.inlinerHTML = `
+<div> ${coders.map((coder) => {document.getElementById("1").delete} )}
+`
+
+function deleteCoder(id) {
+    coders.filter((coder => coder.id !== id))
+} 
+
+//////*RESTART 
+ function newFunction() {
+            document.getElementById("newGame").reset();
+
+/* const btnReset = document.querySelector('btnReset');
+
+btnReset.onclick = function() {
+    document.getElementByClassName('btnReset').reset();
+} */
+
+const buttons = document.getElementsByClassName('btnCoders');
+
+for (let btnCoders of buttons) {
+    btnCoders.addEventListener('click', function() {
+        console.log("KILLED")
+    })
+};
+
+buttons.onclick = function() {
+    console.log("KILLED")
+};
